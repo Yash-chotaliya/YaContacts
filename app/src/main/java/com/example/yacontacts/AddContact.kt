1b39f4e794407268a5d535cc98790df676ebf878
yash chotaliya <yashchotaliya187@gmail.com>
2023-02-28 19:57:24 +0530

    " addcon. imagepicker add "

--- app/src/main/java/com/example/yacontacts/AddContact.kt
+++ app/src/main/java/com/example/yacontacts/AddContact.kt
@@ -1,17 +1,24 @@
 package com.example.yacontacts
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.yacontacts.database.Contacts
 import com.example.yacontacts.database.ContactsViewModel
+import kotlinx.android.synthetic.main.activity_add_contact.*
 import kotlinx.android.synthetic.main.contactrvlay.*
+import kotlinx.android.synthetic.main.contactrvlay.name
+import kotlinx.android.synthetic.main.contactrvlay.number
 
+
+@Suppress("DEPRECATION")
 class AddContact : AppCompatActivity() {
+    private val gallaryPickCode = 100
     private lateinit var viewmodel: ContactsViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +26,26 @@ class AddContact : AppCompatActivity() {
 
         viewmodel = ViewModelProvider(this)[ContactsViewModel::class.java]
 
+        contactpic.setOnClickListener {
+            uploadImage()
+        }
+
+    }
+
+    private fun uploadImage(){
+        val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+        startActivityForResult(gallery, gallaryPickCode)
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(resultCode== RESULT_OK){
+            if(requestCode == gallaryPickCode){
+                contactpic.setImageURI(data?.data)
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
